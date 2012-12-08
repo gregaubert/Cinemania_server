006153df76c3ae12f32bc43c21810d92a6215286
Jorge Albaladejo <jorge@jorge-x60>
2012-12-08 15:05:58 +0100

    more information in profile

--- app/protected/config/main.php
+++ app/protected/config/main.php
@@ -57,7 +57,7 @@ return array(
         // uncomment the following to show log messages on web pages
         array(
           'class'=>'CWebLogRoute',
-          'levels'=>'profile'
+          //'levels'=>'profile'
         ),
       ),
     ),
